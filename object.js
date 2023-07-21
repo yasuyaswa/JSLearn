21ed41a65ad9d38e948b8c4a60c4c9da12deb86c
yaswanth <yasuyaswa@gmail.com>
2023-07-21 10:11:29 +0530

    workspace deleted

--- object.js
+++ object.js
@@ -1,24 +1,52 @@
-var person = {
-  name: "Yaswanth",
-  gender: "Male",
-  age: 23,
-  salary: 50000,
-};
+var person = [
+  {
+    name: "Yaswanth",
+    gender: "Male",
+    age: 23,
+    salary: 50000,
+  },
+  {
+    name: "harita",
+    gender: "Female",
+    age: 22,
+    salary: 28000,
+  },
+  {
+    name: "vinith",
+    gender: "Male",
+    age: 23,
+    salary: 28000,
+  },
+];
 console.log(person.name);
 console.log(person["gender"]);
 delete person.salary;
 console.log(person);
 
-function ObjCheck(CheckProperty) {
-  if (person.hasOwnProperty(CheckProperty)) {
-    return person[CheckProperty];
-  } else {
-    return "not found";
-  }
+function person1(name, gender, age, salary) {
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.salary = salary;
 }
 
-console.log(ObjCheck("name"));
-console.log(ObjCheck("id"));
+var p1 = new person1("vinay", "male", 23, 28000);
+var p2 = new person1("vinay", "female", 22, 34379);
+
+p1.id=4624
+console.log(p1);
+console.log(p2);
+
+// function ObjCheck(CheckProperty) {
+//   if (person.hasOwnProperty(CheckProperty)) {
+//     return person[CheckProperty];
+//   } else {
+//     return "not found";
+//   }
+// }
+
+// console.log(ObjCheck("name"));
+// console.log(ObjCheck("id"));
 
 var vehicles = {
   car: {
