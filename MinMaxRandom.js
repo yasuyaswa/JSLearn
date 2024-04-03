a51b8d075f866838f3fc738c0e2ff7ab93d0a88c
yaswanth <yasuyaswa@gmail.com>
2024-04-03 09:29:55 +0530

    random function edited

--- MinMaxRandom.js
+++ MinMaxRandom.js
@@ -6,3 +6,9 @@ function RandomRange(start, end){
 }
 
 console.log(RandomRange(10,100));
+console.log(Math.max(12,14));
+console.log(Math.min(12,14));
+
+const colors= ['orange', 'green', 'blue', 'grey', 'red'];
+const randomColor= Math.floor(Math.random() * colors.length);
+console.log(colors[randomColor]);
